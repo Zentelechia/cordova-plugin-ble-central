f4835a6a226746e77abb7e012334177db3f0902b
Zentelechia <zentelechia@gmail.com>
2017-11-19 12:35:50 +0300

    .

--- src/android/BLECentralPlugin.java
+++ src/android/BLECentralPlugin.java
@@ -459,7 +459,7 @@ public class BLECentralPlugin extends CordovaPlugin implements BluetoothAdapter.
     }
     private void setDeviceName(CallbackContext callbackContext, String deviceName) {
       LOG.d(TAG, "Called setDeviceName function: " + deviceName);
-      private BluetoothAdapter bA = null;
+      BluetoothAdapter bA = null;
       bA = BluetoothAdapter.getDefaultAdapter();
       bA.setName(deviceName);
       callbackContext.success();
